eb562ea9115f4549ce2b7c8845263746de846cba
eschremp <eschremp93@gmail.com>
2023-10-30 18:27:58 -0500

    completed exercise problems

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,20 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", () => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    })
+    missionAbort.addEventListener("mouseover", () => {
+        missionAbort.style.backgroundColor ="red";    
+    })
+    missionAbort.addEventListener("mouseout", () => {
+        missionAbort.style.backgroundColor ="";
+    })
+    missionAbort.addEventListener("click", () => {
+        if (window.confirm("Are you sure you want to abort the mission?")) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    })
 }
 
 window.addEventListener("load", init);
